e2843c5f87969271f885ff12fb4668adcb35f097
Andrew Quinn <andrew.quinn@psych.ox.ac.uk>
2021-06-25 13:56:15 +0100

    Initial hello world

---
+++ README.md
@@ -0,0 +1,7 @@
+Work-in-progess package for python OSL version.
+
+```
+git clone https://github.com/OHBA-analysis/oslpy.git
+cd oslpy
+pip install .
+```
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='osl',
+      version='0.0.1.dev',
+      description='OHBA Software Library',
+      author='OHBA Analysis Group',
+      license='MIT',
+      packages=['osl'],
+      zip_safe=False)
