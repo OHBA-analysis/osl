efcadd595504d35e54a2ef66e98ab30e27b83060
Mats <mats.vanes@psych.ox.ac.uk>
2021-07-23 15:14:30 +0100

    Update setup.py
    correction for install requirement yaml

--- setup.py
+++ setup.py
@@ -14,6 +14,6 @@ setup(name='osl',
               'osl_maxfilter = osl.maxfilter.maxfilter:main',
               'osl_report = osl.report.raw_report:main',
               ]},
-      install_requires=['mne', 'sklearn', 'fslpy', 'sails', 'tabulate', 'yaml'],
+      install_requires=['mne', 'sklearn', 'fslpy', 'sails', 'tabulate', 'PyYAML'],
       package_data={'osl': ['utils/*tree']},
       )
