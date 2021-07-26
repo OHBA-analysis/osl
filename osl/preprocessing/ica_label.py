3c2a0a19b0d07e5ddeb3b25c3430ac66c0ef0af0
matsvanes <mats.vanes@psych.ox.ac.uk>
2021-07-26 15:46:15 +0100

    add wakehen data to options

--- osl/preprocessing/ica_label.py
+++ osl/preprocessing/ica_label.py
@@ -5,6 +5,7 @@
 def ica_label(dataset):
     global drive, savedir
     import mne
+    import sklearn
     import os
     from matplotlib import pyplot as plt
     plt.ion()
@@ -29,7 +30,7 @@ def ica_label(dataset):
             drive='mwoolrich'
         elif drive==2:
             drive='knobre'
-        drive = "".join(('/ohba/pi/', drive, '/datasets/mrc_meguk/'))
+        drive = "".join(('/ohba/pi/', drive, '/ajquinn/mrc_meguk/processed_data/'))
         #drive = "".join(('/Volumes/T5_OHBA/', drive, '/datasets/mrc_meguk/'))
         # write to text file
         with open(file, "a") as text_file:
@@ -37,7 +38,7 @@ def ica_label(dataset):
 
     if savedir is None:
         researcher_id = input('What are your initials?').lower()
-        savedir = "".join((drive, researcher_id, '/'))
+        savedir = "".join((drive.split('ajquinn')[0], 'ica_label/', researcher_id, '/'))
         with open(file, "a") as text_file:
             print(f"savedir = '{savedir}'", file=text_file)
 
@@ -45,24 +46,47 @@ def ica_label(dataset):
         os.mkdir(savedir)
 
     print(f'### LABELING DATASET {dataset} ###')
+    sub=None
+    if 'oxf' in dataset:
+        site = 'Oxford/'
+    elif 'cam' in dataset:
+        site = 'Camebridge/'
+    elif 'gla' in dataset:
+        site = 'Glasgow/'
+    elif 'not' in dataset:
+        site = 'Nottingham/'
+    elif 'cdf' in dataset:
+        site = 'Cardiff/'
+    elif 'wakehen' in dataset:
+        drive = drive.split('mrc_meguk')[0]
+        dataset = "".join(('sub', dataset.split('wakehen')[1].split('_')[0], '_run_',
+                       dataset.split('wakehen')[1].split('_')[1], '_sss'))
+        site = 'WakeHen/processed/'
+
     print('LOADING DATA')
-    raw = mne.io.read_raw("".join((drive,dataset, '_raw.fif')))
-    ica = mne.preprocessing.read_ica("".join((drive,dataset, '_ica.fif')))
+    raw = mne.io.read_raw("".join((drive, site, dataset, '_raw.fif')))
+    ica = mne.preprocessing.read_ica("".join((drive, site, dataset, '_ica.fif')))
     ica.exclude = [] # empty automatic labeling info
     print(ica.exclude)
 
     # copy ica to researcher specific folder
     print('COPYING DATA TO RESEARCHER SPECIFIC FOLDER')
-    ica.save("".join((savedir, dataset, '_ica.fif')))
+    if 'WakeHen' in site:
+        site = 'WakeHen/'
+    try:
+        os.mkdir("".join((savedir, site)))
+    except:
+        print('')
+    ica.save("".join((savedir, site, dataset, '_ica.fif')))
 
     # interactive components plot
     print('INTERACTIVE ICA LABELING')
-    from osl_plot_ica import plot_ica
+    from osl.preprocessing.osl_plot_ica import plot_ica
     plot_ica(ica,raw, block=True)
     plt.pause(0.1)
 
     print('SAVING DATA')
-    ica.save("".join((savedir, dataset, '_ica.fif')))
+    ica.save("".join((savedir, site, dataset, '_ica.fif')))
     del ica, raw
     print(f'### LABELING DATASET {dataset} COMPLETE ###')
 
