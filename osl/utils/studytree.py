3d57c2a04aecff976814c7ef550929b67a43e811
Chetan Gohil <chetan.gohil@psych.ox.ac.uk>
2021-10-31 10:04:40 +0000

    Added more info if an error is raised in StudyTree.

--- osl/utils/studytree.py
+++ osl/utils/studytree.py
@@ -11,12 +11,16 @@ class StudyTree:
 
     def __init__(self, treename, study_dir):
         tree_directories.append(os.path.dirname(inspect.getfile(inspect.currentframe())))
+        self.study_dir = study_dir
         self.tree = FileTree.read(treename, study_dir)
         self.query = FileTreeQuery(self.tree)
 
     def get(self, template, asmatch=False, strip_fname=False, **kwargs):
         if template not in self.query.templates:
-            raise ValueError
+            raise ValueError(
+                "template not found. Available: {0}\n".format(" ".join(self.query.templates))
+                + "Using base directory: {0}".format(self.study_dir)
+            )
 
         qq = self.query.query(template, **kwargs)
         if asmatch is False:
