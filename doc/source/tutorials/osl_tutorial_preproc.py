ea050c61af3c36c73af828d38e48fcaf10526647
Andrew Quinn <andrew.quinn@psych.ox.ac.uk>
2022-01-31 18:06:43 +0000

    Add bad channel and segment detection to simple simulation

--- doc/source/tutorials/osl_tutorial_preproc.py
+++ doc/source/tutorials/osl_tutorial_preproc.py
@@ -22,3 +22,34 @@ raw.plot(n_channels=30)
 # This generates some fairly realisatic MEG data with prominant alpha oscillations.
 
 raw.plot_psd()
+
+#%%
+# Make data with some problems...
+
+flat_channels = [10]
+bad_channels = [5, 200]
+bad_segments = [(600, 750)]
+
+raw = osl.utils.simulate_raw_from_template(5000,
+                                           flat_channels=flat_channels,
+                                           bad_channels=bad_channels,
+                                           bad_segments=bad_segments)
+
+raw.plot(n_channels=30)
+
+#%%
+
+bad_annotations, flat_channels = mne.preprocessing.annotate_flat(raw)
+
+raw.set_annotations(bad_annotations)
+raw.info['bads'].extend(flat_channels)
+
+#%%
+
+raw.plot(n_channels=30)
+
+#%%
+
+raw = osl.preprocessing.batch.detect_badsegments(raw, segment_len=150)
+
+raw.plot(n_channels=30)
