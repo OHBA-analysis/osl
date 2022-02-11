f43cf57f35cae03e93e7fb8d1c47569f781bb51b
woolrich <woolrich@fmrib.ox.ac.uk>
2022-02-11 14:23:50 +0000

    Included rhino and rhino_utils for import

--- osl/rhino/__init__.py
+++ osl/rhino/__init__.py
@@ -2,6 +2,8 @@
 
 # vim: set expandtab ts=4 sw=4:
 
+from . import rhino # noqa: F401, F403
+from . import rhino_utils # noqa: F401, F403
 from . import examples  # noqa: F401, F403
 
 # Set logger to only show warning/critical messages
