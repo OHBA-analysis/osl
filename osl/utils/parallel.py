ef8d41e096579a34d00768834b921211dad4b9c3
Andrew Quinn <andrew.quinn@psych.ox.ac.uk>
2022-04-21 12:40:38 +0100

    Finish first pass at dask cluster backend

--- osl/utils/parallel.py
+++ osl/utils/parallel.py
@@ -1,7 +1,7 @@
 #import multiprocessing as mp
 #import multiprocess as mp
 from functools import partial
-from dask.distributed import Client, LocalCluster
+from dask.distributed import Client, LocalCluster, wait
 import dask
 
 # Housekeeping for logging
@@ -32,69 +32,28 @@ def initialise_pool(nprocesses=1):
     return P
 
 
-def initialise_client(**kwargs):
-    """Initialise a Dask client for massive parallel processing.
+def dask_parallel(client, func, iter_args,
+                  func_args=None, func_kwargs=None,
+                  block_console=True,
+                  ret_results=False):
 
-    This will always start a new cluster and client connection.
+    func_args = [] if func_args is None else func_args
+    func_kwargs = {} if func_kwargs is None else func_kwargs
 
-    """
-    # In general, we probably want lots of processes with relatively few theads
-    # per process - this is as we're probably running complex code on MNE data
-    # objects rather than pure numpy array operations.
-    #
-    # If address is not passed, Client creates a cluster using LocalCluster
-    # otherwise client will connect to whatever is at the end of the address.
-    #
-    # Optional environment config to limit threads in processes
-    #export OMP_NUM_THREADS=1
-    #export MKL_NUM_THREADS=1
-    #export OPENBLAS_NUM_THREADS=1
+    osl_logger.info('Dask Client : {0}'.format(client.__repr__()))
+    osl_logger.info('Dask Client dashboard link: {0}'.format(client.dashboard_link))
 
-    kwargs.setdefault('processes', True)
-    kwargs.setdefault('threads_per_worker', 1)
-    kwargs.setdefault('name', 'osl-cluster')
-    kwargs.setdefault('scheduler_port', 8786)  # This is the dask-default
-
-    if _dask_find_existing(scheduler_port=kwargs['scheduler_port']) is not None:
-        osl_logger.error('Found Dask client running in this session')
-        osl_logger.error('Either use existing client/cluster or close before starting a new instance')
-
-    osl_logger.info('Starting new Dask Cluster : {0}'.format(kwargs))
-    cluster = LocalCluster(scheduler_port=kwargs['scheduler_port'])
-    client = Client(cluster)
-
-    osl_logger.info('Cluster dashboard link: {0}'.format(client.dashboard_link))
-
-    return client
-
-
-def _dask_find_existing(scheduler_port=8786):
-    try:
-        client = Client(f'tcp://localhost:{scheduler_port}', timeout='2s')
-    except OSError:
-        client = None
-    return client
-
-
-def _close_client_and_cluster(client):
-    osl_logger.info('Closing dask client: {0}'.format(client))
-    if client.cluster is not None:
-        client.cluster.close()
-    client.close()
-
-
-def cluster_run_delayed(client, func, iter_args, *args, **kwargs):
-
-    osl_logger.debug('User args : {0}'.format(args))
-    osl_logger.debug('User kwargs : {0}'.format(kwargs))
+    osl_logger.debug('Running function : {0}'.format(func.__repr__()))
+    osl_logger.debug('User args : {0}'.format(func_args))
+    osl_logger.debug('User kwargs : {0}'.format(func_kwargs))
 
     # Set kwargs - need to handle args on function call to preserve order.
-    run_func = partial(func, **kwargs)
+    run_func = partial(func, **func_kwargs)
 
     # Cue up jobs
     lazy_results = []
     for idx, aa in enumerate(iter_args):
-        lazy_results.append(dask.delayed(run_func)(aa, *args))
+        lazy_results.append(dask.delayed(run_func)(aa, *func_args))
     osl_logger.info('Prepared {0} jobs for processing'.format(len(lazy_results)))
 
     #persist or compute? probably persist
@@ -102,5 +61,15 @@ def cluster_run_delayed(client, func, iter_args, *args, **kwargs):
     osl_logger.info('Starting computation')
     futures = dask.persist(*lazy_results)  # trigger computation in the background
 
-    osl_logger.info('Complete')
+    # This should probably always be True
+    if block_console:
+        wait(futures)
+
+    osl_logger.info('Computation complete')
+
+    if ret_results:
+        osl_logger.info('Gathering results')
+        results = client.gather(futures)
+        return results
+
     return futures
