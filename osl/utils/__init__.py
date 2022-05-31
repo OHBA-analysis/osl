28cc786d872617a5ee30c7a3c4c3c8d7274f8cc3
Andrew Quinn <andrew.quinn@psych.ox.ac.uk>
2022-05-31 15:52:43 +0100

    Make open3d a soft import in rhino submodule. Add soft_import function to utils

--- osl/utils/__init__.py
+++ osl/utils/__init__.py
@@ -8,3 +8,4 @@ from .studytree import StudyTree  # noqa: F401, F403
 from .file_handling import *  # noqa: F401, F403
 from .spmio import SPMMEEG  # noqa: F401, F403
 from .simulate import *  # noqa: F401, F403
+from .package import soft_import
