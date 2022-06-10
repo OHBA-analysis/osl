7bcce273559b5b1d2fcd1af9739ce184ee5c8c83
Chetan Gohil <chetan.gohil@psych.ox.ac.uk>
2022-06-10 13:10:26 +0100

    Removed duplicate file created from renaming (mne_wrappers) and fixed import error.

--- osl/utils/__init__.py
+++ osl/utils/__init__.py
@@ -3,6 +3,7 @@
 from . import simulate  ## noqa: F401, F403
 from . import logger  ## noqa: F401, F403
 from .study import Study  # noqa: F401, F403
+from .studytree import StudyTree  # noqa: F401, F403
 from .file_handling import *  # noqa: F401, F403
 from .spmio import SPMMEEG  # noqa: F401, F403
 from .parallel import dask_parallel_bag  # noqa: F401, F403
