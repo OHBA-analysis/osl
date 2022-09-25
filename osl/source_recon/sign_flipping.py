Fixed bug in finding the median subject in sign flipping.

osl/source_recon/sign_flipping.py
@@ -165,9 +165,9 @@ def find_template_subject(covs, diag_offset=0):
 
     # Get the median subject
     metric_sum = np.sum(metric, axis=1)
-    median = np.median(metric_sum)
+    argmedian = np.argsort(metric_sum)[len(metric_sum) // 2]
 
-    return np.argwhere(metric_sum == median)[0][0]
+    return argmedian
 
 
 def covariance_matrix_correlation(M1, M2, diag_offset=0, mode=None):