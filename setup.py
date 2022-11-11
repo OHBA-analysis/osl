35829dec28b0bdef838e5c70bee8a55be708890d
Andrew Quinn <aquinn501@gmail.com>
2022-11-11 16:24:58 +0000

    reorganise setup.py and add pacakge long description from README

--- setup.py
+++ setup.py
@@ -1,4 +1,12 @@
+import pathlib
+
 from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
 
 # Requirement categories
 reqs = ['numpy', 'scipy', 'matplotlib', 'mne>=1.0.0', 'sklearn', 'fslpy',
@@ -13,12 +21,38 @@ name = 'osl'
 setup(name=name,
       version='0.0.1.dev',
       description='OHBA Software Library',
+      long_description=README,
+      long_description_content_type="text/markdown",
       author='OHBA Analysis Group',
       license='MIT',
-      packages=['osl', 'osl.report', 'osl.maxfilter',
-                'osl.preprocessing', 'osl.utils', 'osl.utils.spmio',
-                'osl.source_recon', 'osl.source_recon.rhino',
-                'osl.source_recon.parcellation'],
+
+      # Choose your license
+      # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+          'Development Status :: 4 - Beta',
+
+          # Indicate who your project is intended for
+          'Intended Audience :: Science/Research',
+          'Topic :: Scientific/Engineering :: Bio-Informatics',
+          'Topic :: Scientific/Engineering :: Information Analysis',
+          'Topic :: Scientific/Engineering :: Mathematics',
+
+          # Specify the Python versions you support here. In particular, ensure
+          # that you indicate whether you support Python 2, Python 3 or both.
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: 3.8',
+          'Programming Language :: Python :: 3.9',
+          'Programming Language :: Python :: 3.10',
+      ],
+
+      python_requires='>=3.7',
+      install_requires=reqs,
+      extras_require={
+          'dev': dev_reqs,
+          'doc': doc_reqs,
+          'full': dev_reqs + doc_reqs,
+      },
 
       zip_safe=False,
       entry_points={
@@ -28,13 +62,11 @@ setup(name=name,
               'osl_report = osl.report.raw_report:main',
           ]},
 
-      python_requires='>=3.7',
-      install_requires=reqs,
-      extras_require={
-          'dev': dev_reqs,
-          'doc': doc_reqs,
-          'full': dev_reqs + doc_reqs,
-      },
+      packages=['osl', 'osl.report', 'osl.maxfilter',
+                'osl.preprocessing', 'osl.utils', 'osl.utils.spmio',
+                'osl.source_recon', 'osl.source_recon.rhino',
+                'osl.source_recon.parcellation'],
+
 
       package_data={'osl': ['utils/*tree',
                             'utils/simulation_config/*npy',
