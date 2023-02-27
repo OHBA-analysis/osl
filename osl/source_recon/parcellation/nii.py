2cba6c345047eedb65b4633c1bc87a59579d953c
woolrich <woolrich@fmrib.ox.ac.uk>
2023-02-27 14:05:36 +0000

    message

--- osl/source_recon/parcellation/nii.py
+++ osl/source_recon/parcellation/nii.py
@@ -19,31 +19,35 @@ import os
 import os.path as op
 from osl.source_recon import parcellation
 
-workingdir = '/Users/woolrich/oslpy/osl/source_recon/parcellation/files/'
+workingdir = '/Users/woolrich/osl/osl/source_recon/parcellation/files/'
 parc_name = 'Schaefer2018_100Parcels_7Networks_order_FSLMNI152_2mm'
+
+os.system('fslmaths /Users/woolrich/Downloads/{} {}'.format(parc_name, workingdir))
+
 tmpdir = op.join(workingdir, 'tmp')
-#os.mkdir(tmpdir)
+os.mkdir(tmpdir)
 parcel3d_fname = op.join(workingdir, parc_name + '.nii.gz')
 parcel4d_fname = op.join(workingdir, parc_name + '_4d.nii.gz')
 parcellation.nii.convert_3dparc_to_4d(parcel3d_fname, parcel4d_fname, tmpdir, 100)
 
-mni_file = '/Users/woolrich/oslpy/osl/source_recon/parcellation/files/MNI152_T1_8mm_brain.nii.gz'
+mni_file = '/Users/woolrich/osl/osl/source_recon/parcellation/files/MNI152_T1_8mm_brain.nii.gz'
 spatial_res = 8 # mm
 parcel4d_ds_fname = op.join(workingdir, parc_name + '_4d_ds' + str(spatial_res) + '.nii.gz')
 parcellation.nii.spatially_downsample(parcel4d_fname, parcel4d_ds_fname, mni_file, spatial_res)
 
 #############
 
-os.system('fslmaths /Users/woolrich/oslpy/osl/source_recon/parcellation/files/HarvardOxford-sub-prob-2mm.nii.gz -thr 50 -bin /Users/woolrich/oslpy/osl/source_recon/parcellation/files/HarvardOxford-sub-prob-bin-2mm.nii.gz')
-file_in = '/Users/woolrich/oslpy/osl/source_recon/parcellation/files/Schaefer2018_100Parcels_7Networks_order_FSLMNI152_2mm_4d.nii.gz'
-file_out = '/Users/woolrich/oslpy/osl/source_recon/parcellation/files/HarvOxf-sub-Schaefer100-combined-2mm_4d.nii.gz'
-file_append = '/Users/woolrich/oslpy/osl/source_recon/parcellation/files/HarvardOxford-sub-prob-bin-2mm.nii.gz'
-parcel_indices = [3,4,5,6,7,8,9,10,14,15,16,17,18,19,20]
+os.system('fslmaths /usr/local/fsl/data/atlases/HarvardOxford/HarvardOxford-sub-prob-2mm.nii.gz -thr 50 -bin /Users/woolrich/osl/osl/source_recon/parcellation/files/HarvardOxford-sub-prob-bin-2mm.nii.gz')
+
+file_in = '/Users/woolrich/osl/osl/source_recon/parcellation/files/Schaefer2018_100Parcels_7Networks_order_FSLMNI152_2mm_4d.nii.gz'
+file_out = '/Users/woolrich/osl/osl/source_recon/parcellation/files/HarvOxf-sub-Schaefer100-combined-2mm_4d.nii.gz'
+file_append = '/Users/woolrich/osl/osl/source_recon/parcellation/files/HarvardOxford-sub-prob-bin-2mm.nii.gz'
+parcel_indices = [3,4,5,6,8,9,10,14,15,16,17,18,19,20] # index from 0
 parcellation.nii.append_4d_parcellation(file_in, file_out, file_append, parcel_indices)
 
-parc_name = '/Users/woolrich/oslpy/osl/source_recon/parcellation/files/HarvOxf-sub-Schaefer100-combined-2mm_4d'
+parc_name = '/Users/woolrich/osl/osl/source_recon/parcellation/files/HarvOxf-sub-Schaefer100-combined-2mm_4d'
 parcel4d_fname = op.join(parc_name + '.nii.gz')
-mni_file = '/Users/woolrich/oslpy/osl/source_recon/parcellation/files/MNI152_T1_8mm_brain.nii.gz'
+mni_file = '/Users/woolrich/osl/osl/source_recon/parcellation/files/MNI152_T1_8mm_brain.nii.gz'
 spatial_res = 8 # mm
 parcel4d_ds_fname = op.join(parc_name + '_ds' + str(spatial_res) + '.nii.gz')
 parcellation.nii.spatially_downsample(parcel4d_fname, parcel4d_ds_fname, mni_file, spatial_res)
