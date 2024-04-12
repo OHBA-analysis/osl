36d7a956b9cddaf1b78a595cda8428847b88114d
cgohil <cgohil@hbaws45.localdomain>
2024-04-12 16:24:46 +0100

    New dev version.

--- osl/__init__.py
+++ osl/__init__.py
@@ -37,4 +37,4 @@ osl_logger = logging.getLogger(__name__)
 osl_logger.debug('osl main init complete')
 
 # --------------------------------------------------------
-__version__ = '0.6.0'
+__version__ = '0.7.dev0'
