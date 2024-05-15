19b3cbb1e8c49ac2d99e2576fd0e883693c658d6
Chetan Gohil <chetan.gohil@psych.ox.ac.uk>
2024-05-15 10:03:39 +0100

    Updated script to fix sMRI files.

--- examples/miscellaneous/fix_smri_files.py
+++ examples/miscellaneous/fix_smri_files.py
@@ -4,12 +4,17 @@ This script uses FSL to set the sform code of any structural
 whose sform code is not 1 or 4 to make sure it is compatible
 with OSL.
 
-Warning: this script will permanently change the SMRI file.
+This script will first create a copy of the sMRI then change
+the sform code.
 """
 
 import nibabel as nib
 
 from osl import source_recon
+
+def run(cmd):
+    print(cmd)
+    source_recon.rhino.utils.system_call(cmd)
 
 # Paths to files to fix
 files = [
@@ -17,11 +22,16 @@ files = [
     "smri/sub-002.nii.gz",
 ]
 
+# Make output directory
+output_directory = "data/smri"
+run(f"mkdir -p {output_dir}")
+
 for file in files:
-    smri = nib.load(file)
-    sformcode = smri.header.get_sform(coded=True)[-1]
-    if sformcode not in [1, 4]:
-        cmd = f"fslorient -setsformcode 1 {file}"
-        source_recon.rhino.utils.system_call(cmd)
+    # Copy the original file
+    run(f"cp {file} {output_dir}")
+    file = f"{output_dir}/{file}"
+
+    # Set the sform code to 1
+    run(f"fslorient -setsformcode 1 {file}")
 
 print("Done")
