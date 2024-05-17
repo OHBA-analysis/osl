ba29d43dc5b1f229752823843af27abb7eb1a1a0
Chetan Gohil <chetan.gohil@psych.ox.ac.uk>
2024-05-17 09:45:08 +0100

    New dev version.

--- osl/__init__.py
+++ osl/__init__.py
@@ -37,4 +37,4 @@ osl_logger = logging.getLogger(__name__)
 osl_logger.debug('osl main init complete')
 
 # --------------------------------------------------------
-__version__ = '0.7.dev0'
+__version__ = '0.8.dev0'
